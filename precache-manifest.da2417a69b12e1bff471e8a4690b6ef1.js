9982e6b2f4581297e56d43d9ff7eb6464e2972d5
Surajram.S <surajram1707@gmail.com>
2020-09-14 14:10:52 +0530

    Updates

---
+++ precache-manifest.da2417a69b12e1bff471e8a4690b6ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1466b11573b6ce3b982fc526778dc5",
+    "url": "/My_First_React_App/index.html"
+  },
+  {
+    "revision": "e78501d964b8c3b59b59",
+    "url": "/My_First_React_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/My_First_React_App/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_First_React_App/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78501d964b8c3b59b59",
+    "url": "/My_First_React_App/static/js/main.8d80077c.chunk.js"
+  },
+  {
+    "revision": "6bd0c5c9ee37073209e4",
+    "url": "/My_First_React_App/static/js/runtime-main.2f153c82.js"
+  }
+]);
